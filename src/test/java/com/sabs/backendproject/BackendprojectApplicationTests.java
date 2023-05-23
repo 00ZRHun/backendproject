fa0d28e4e0928e3ad1ab346bfdb0a3f983dbd1d6
zuronghun <128782538+zuronghun@users.noreply.github.com>
2023-05-23 17:21:16 +0800

    feat: setup Spring Boot with Postgres database

---
+++ src/test/java/com/sabs/backendproject/BackendprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabs.backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
