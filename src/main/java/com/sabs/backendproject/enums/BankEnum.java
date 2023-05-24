c22b9d6a2ef5548194a32733524494957eb9861f
zuronghun <128782538+zuronghun@users.noreply.github.com>
2023-05-24 15:29:18 +0800

    feat: add enum for BankEnum & PaymentMethodEnum

---
+++ src/main/java/com/sabs/backendproject/enums/BankEnum.java
@@ -0,0 +1,23 @@
+package com.sabs.backendproject.enums;
+
+public enum BankEnum {
+
+  AFFIN,
+  ALLIANCE,
+  AMBANK,
+  BSN,
+  CIMB,
+  CITIBANK,
+  DIRECT_BANK_IN,
+  EON,
+  HLB,
+  HSBC,
+  MBB,
+  OCBC,
+  PBB,
+  RHB,
+  STANCHART,
+  TNG_EWALLET,
+  UOB
+  
+}
---
+++ src/main/java/com/sabs/backendproject/enums/PaymentMethodEnum.java
@@ -0,0 +1,9 @@
+package com.sabs.backendproject.enums;
+
+public enum PaymentMethodEnum {
+
+  CREDIT_CARD,
+  CASH,
+  CHEQUE
+
+}
